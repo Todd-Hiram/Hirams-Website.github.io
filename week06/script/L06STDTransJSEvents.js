d13078913c8a42464a1bad06240bcebcd7653b4c
Todd-Hiram <htillustrator@gmail.com>
2018-05-28 21:12:23 -0700

    L06 Revisions 05/28/2018

--- week06/script/L06STDTransJSEvents.js
+++ week06/script/L06STDTransJSEvents.js
@@ -1,31 +1,21 @@
-function theSphere() {
-    let sphere = document.getElementById(sphere);
-    sphere.className = (sphere);
-    sphere.ondrag = handleDrag;
-    sphere.setAttribute('draggable', 'true');
-    sphere.addEventListener('touchmove', handleTouchMove);
-    document.body.appendChild(sphere);
-}
 
-function handleDrag(event) {
-    // The horizontal coordinate
-    let x = event.clientX; 
-    // The vertical coordinate
-    let y = event.clientY;
-    
-    console.log("drag", x, y);
-    
-    if (x && y) {
-        this.style.left = (x - 25) + "px";
-        this.style.top = (y - 25) + "px";
-    }
-}
-
-function handleTouchMove(event) {
-    let touch = event.targetTouches[0];
-    
-    // This places the element where the the finger is located on screen
-    this.style.left = touch.clientX - 25 + "px";
-    this.style.top = touch.clientY - 25 + "px";
+function allowDrop(event) {
     event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
+
+
+function touchAndMove(event) {
+    let x = event.touches[0].clientX;
+    let y = event.touches[0].clientY;
+    event.target.appendChild(document.getElementById(data));
 }
