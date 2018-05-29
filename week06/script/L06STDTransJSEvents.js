39bd14c512bd0c5d648c92af600b477e322756cf
Todd-Hiram <htillustrator@gmail.com>
2018-05-28 22:40:06 -0700

    Revisions

--- week06/script/L06STDTransJSEvents.js
+++ week06/script/L06STDTransJSEvents.js
@@ -13,13 +13,6 @@ function drop(event) {
     event.target.appendChild(document.getElementById(data));
 }
 
-
-/*function touchAndMove(event) {
-    let x = event.touches[0].clientX;
-    let y = event.touches[0].clientY;
-    event.target.appendChild(document.getElementById(data));
-}*/
-
 function myFunction(event) {
   var x = event.touches[0].clientX;
   var y = event.touches[0].clientY;
