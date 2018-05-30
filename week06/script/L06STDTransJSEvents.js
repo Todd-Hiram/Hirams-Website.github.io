e6daefabf34e8500558b3b5db9f3584afa5ef4f7
Todd-Hiram <htillustrator@gmail.com>
2018-05-30 14:58:46 -0700

    Revisions 05/30/2018
    Added mousemove and mouseout to L06 site page.

--- week06/script/L06STDTransJSEvents.js
+++ week06/script/L06STDTransJSEvents.js
@@ -1,4 +1,4 @@
-
+/*------------ Drag and Drop ------------*/
 function allowDrop(event) {
     event.preventDefault();
 }
@@ -13,45 +13,23 @@ function drop(event) {
     event.target.appendChild(document.getElementById(data));
 }
 
-function touchAndMove(touch) {
-  var x = touch.touches[0].clientX;
-  var y = touch.touches[0].clientY;
-  touch.target.appendChild(document.getElementById(data));
-}
 
+/*------------ Drag Finger for Coordinates ------------*/
 function myFunction(event) {
-  var x = event.touches[0].clientX;
-  var y = event.touches[0].clientY;
-  document.getElementById("demo").innerHTML = x + ", " + y;
+  let x = event.touches[0].clientX;
+  let y = event.touches[0].clientY;
+  document.getElementById("demo1").innerHTML = x + ", " + y;
 }
 
-/*--------------------------------- latest -----------------------------*/
-/*function myFunction() {
-    x.style.WebkitAnimation = "mymove 4s 2"; // Code for Chrome, Safari and Opera
-    x.style.animation = "mymove 4s 2";       // Standard syntax
+
+/*------------ Drag Mouse for Coordinates ------------*/
+function mouseMove(mouseEvent) {
+    let x = mouseEvent.clientX;
+    let y = mouseEvent.clientY;
+    let coordinates = "Coordinate are: (" + x + ", " + y + ")";
+    document.getElementById("demo2").innerHTML = coordinates;
 }
 
-// Code for Chrome, Safari and Opera
-x.addEventListener("webkitAnimationStart", myStartFunction);
-x.addEventListener("webkitAnimationIteration", myRepeatFunction);
-x.addEventListener("webkitAnimationEnd", myEndFunction);
-
-// Standard syntax
-x.addEventListener("animationstart", myStartFunction);
-x.addEventListener("animationiteration", myRepeatFunction);
-x.addEventListener("animationend", myEndFunction);
-
-function myStartFunction() {
-    this.innerHTML = "animationstart event occured - The animation has started";
-    this.style.backgroundColor = "pink";
+function clearCoord() {
+    document.getElementById("demo2").innerHTML = "";
 }
-
-function myRepeatFunction() {
-    this.innerHTML = "animationiteration event occured - The animation was played again";
-    this.style.backgroundColor = "lightblue";
-}
-
-function myEndFunction() {
-    this.innerHTML = "animationend event occured - The animation has completed";
-    this.style.backgroundColor = "lightgray";
-}*/
