24e147476225083186eb389543c9db68f18c5dd0
Todd-Hiram <htillustrator@gmail.com>
2018-05-29 23:46:02 -0700

    L06 Revisions 05/29/2018

--- week06/script/L06STDTransJSEvents.js
+++ week06/script/L06STDTransJSEvents.js
@@ -24,3 +24,34 @@ function myFunction(event) {
   var y = event.touches[0].clientY;
   document.getElementById("demo").innerHTML = x + ", " + y;
 }
+
+/*--------------------------------- latest -----------------------------*/
+function myFunction() {
+    x.style.WebkitAnimation = "mymove 4s 2"; // Code for Chrome, Safari and Opera
+    x.style.animation = "mymove 4s 2";       // Standard syntax
+}
+
+// Code for Chrome, Safari and Opera
+x.addEventListener("webkitAnimationStart", myStartFunction);
+x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+x.addEventListener("webkitAnimationEnd", myEndFunction);
+
+// Standard syntax
+x.addEventListener("animationstart", myStartFunction);
+x.addEventListener("animationiteration", myRepeatFunction);
+x.addEventListener("animationend", myEndFunction);
+
+function myStartFunction() {
+    this.innerHTML = "animationstart event occured - The animation has started";
+    this.style.backgroundColor = "pink";
+}
+
+function myRepeatFunction() {
+    this.innerHTML = "animationiteration event occured - The animation was played again";
+    this.style.backgroundColor = "lightblue";
+}
+
+function myEndFunction() {
+    this.innerHTML = "animationend event occured - The animation has completed";
+    this.style.backgroundColor = "lightgray";
+}
