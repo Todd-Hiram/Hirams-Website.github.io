b94c8216b3d7c7ed43ce6158e8043925ef9ce07c
Todd-Hiram <htillustrator@gmail.com>
2018-07-12 23:52:40 -0700

    Nav Rev 07/12/2018

--- FinalProjectApp/termWebsiteProject/js/hamburger.js
+++ FinalProjectApp/termWebsiteProject/js/hamburger.js
@@ -1,5 +1,29 @@
-function toggleHam() {
+/*function toggleHam() {
     'use strict';
 document.getElementsByClassName("navigation")[0]
 .classList.toggle("responsive");
+}
+
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "navigation") {
+        x.className += " responsive";
+    } else {
+        x.className = "navigation";
+    }
+}*/
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
