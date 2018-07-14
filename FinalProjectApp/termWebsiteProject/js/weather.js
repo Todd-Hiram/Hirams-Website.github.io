f74979fdb34a279e4aeb48d61903ccead524fb0d
Todd-Hiram <htillustrator@gmail.com>
2018-07-14 00:47:40 -0700

    Revisions 07/12/2018
    Trying to resolve the 'us strict'; for zipcode weather.

--- FinalProjectApp/termWebsiteProject/js/weather.js
+++ FinalProjectApp/termWebsiteProject/js/weather.js
@@ -1,8 +1,9 @@
-let httpRequest;
-
+var httpRequest;
 function fetchWeatherData() {
     'use strict';
-    let zipCode = document.querySelector("input[name='zipCode']").value;
+
+    var zipCode = document.querySelector("input[name='zipCode']").value;
+    
     if (zipCode) {
         console.log("zipCode:", zipCode);
         httpRequest = new XMLHttpRequest();
@@ -10,6 +11,7 @@ function fetchWeatherData() {
         if (!httpRequest) {
             return;
         }
+        
         httpRequest.onreadystatechange = parseWeatherData;
         httpRequest.open('GET', 'https://api.wunderground.com/api/3b345a3fea42ac03/conditions/q/' + zipCode + '.json');
         httpRequest.send();
@@ -18,19 +20,22 @@ function fetchWeatherData() {
 
 function parseWeatherData() {
     console.log("parsing");
+    
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        
         if (httpRequest.status === 200) {
+            
             try {
-                let data = JSON.parse(httpRequest.responseText);
+                var data = JSON.parse(httpRequest.responseText);
                 console.log(data);
-                document.querySelector("#city-label").innerText            = data["current_observation"]["display_location"]["full"];
+                document.querySelector("#city-label").innerText = data["current_observation"]["display_location"]["full"];
                 document.querySelector("#current-weather-label").innerText = data["current_observation"]["weather"];
-            }
-            catch (e) {
+            } catch (e) {
                 console.log(e);
                 console.log(httpRequest.responseText);
                 alert("The response was not valid JSON.");
             }
+            
         } else {
             alert('There was a problem with the request.');
         }
